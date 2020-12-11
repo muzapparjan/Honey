2bc5083fcadd0c482c4c5fa0c1ab8b00ca98fa11
979697473@qq.com <979697473@qq.com>
2020-12-11 20:57:58 +0800

    补充协议

--- Honey.Core/Lexer.cs
+++ Honey.Core/Lexer.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/**
+ * HUST-CS1801-Muzappar
+ * 2020-12-11
+ * ----------------------------------------------------------------------------------------------------
+ * Agreement Block / 协议区
+ * 
+ * 1.You can do any legal things you want with this software.
+ * 1.你可以用这个软件做任何合法的事情。
+ * 
+ * 2.I'm not responsible for anything.
+ * 2.我不负任何责任。
+ * 
+ * 3.You have to pay me 6.66 China Yuan to use all of this software.My AliPay account is 17625860648.
+ * 3.你得付给我6.66元人民币才能使用整个软件。我的支付宝账户为17625860648。
+ * ----------------------------------------------------------------------------------------------------
+ */
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 
